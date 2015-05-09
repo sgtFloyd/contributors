ffe9395d40a61685020ce3d44d575ee3fa170816
Gabe <gabe@paperlesspost.com>
2015-05-09 13:35:57 -0400

    include recent pull requests

--- app/main.rb
+++ app/main.rb
@@ -11,19 +11,36 @@ class MainApp < Sinatra::Base
   enable :sessions
 
   helpers do
-    def github(options={})
-      @_github ||= Github.new(API_PARAMS.merge(options))
+    def authorized?
+      !!session[:token]
     end
 
     def require_authorization
-      redirect to('/login') unless !!session[:token]
+      redirect to('/login') unless authorized?
+    end
+
+    def github(options={})
+      options[:oauth_token] ||= session[:token] if authorized?
+      @_github ||= Github.new(API_PARAMS.merge(options))
     end
 
     def parse_github_link(link)
-      link_re = /github\.com\/(?<user>[^\/]+)\/(?<repo>[^\/]+)/
       match = link.to_s.match(/github\.com\/(?<user>[^\/]+)\/(?<repo>[^\/]+)/)
       raise ArgumentError unless match[:user] && match[:repo]
       [match[:user], match[:repo]]
+    end
+
+    def top_contributors_for(repository, limit: 5)
+      contributors = github.repos.contributors(repository.owner.login, repository.name)
+      contributors.first(limit).map{|contributor| github.users.get(user: contributor.login)}
+    end
+
+    def pull_requests_for(repository, limit: 20)
+      filter = { state: 'closed', sort: 'updated' }
+      pull_requests = github.pulls.list(repository.owner.login, repository.name, filter)
+      pull_requests.first(limit).each do |pull_request|
+        pull_request.user = github.users.get(user: pull_request.user.login)
+      end
     end
   end
 
@@ -32,13 +49,13 @@ class MainApp < Sinatra::Base
     haml :index
   end
 
+  # TO INCLUDE: Organizations, Hirable
   get '/search' do
     require_authorization
     user, repo = parse_github_link(params[:q])
-    @repository = github(oauth_token: session[:token]).repos.get(user, repo)
-    @contributors = github.repos.contributors(user, repo).map do |contributor|
-      github.users.get user: contributor.login
-    end
+    @repository = github.repos.get(user, repo)
+    @top_contributors = top_contributors_for @repository
+    @pull_requests = pull_requests_for @repository
     haml :show
   end
 
